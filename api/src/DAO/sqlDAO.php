c02a47633f1a31f861e24955ce13359e110b7a36
Pacific <mupacif@gmail.com>
2016-12-02 18:08:35 +0100

    v1

--- api/src/DAO/sqlDAO.php
+++ api/src/DAO/sqlDAO.php
@@ -23,22 +23,22 @@ class sqlDAO
 
         public function insertMat($idDom,$name)
     {
-         $sql = "insert into matiere(refDom,name,savedOn) value (?,?,NOW())";
+         $sql = "insert into Matiere(refDom,name,savedOn) value (?,?,NOW())";
           $result = $this->db->executeQuery($sql,[$idDom,$name]);
            return (int)$this->db->fetchArray("SELECT LAST_INSERT_ID();")[0];
     }
 
          public function insertChap($idMat,$name)
     {
-         $sql = "insert into chapitre(refMat,name,savedOn) value (?,?,NOW())";
+         $sql = "insert into Chapitre(refMat,name,savedOn) value (?,?,NOW())";
           $result = $this->db->executeQuery($sql,[$idMat,$name]);
            return (int)$this->db->fetchArray("SELECT LAST_INSERT_ID();")[0];
     }
 
     public function getAll()
     {
-         $sql ="select domain.id as idDom,domain.name as nameDom,matiere.id as idMat,matiere.name as nameMat,chapitre.id as idChap,chapitre.name as nameChap from domain RIGHT join matiere on domain.id=matiere.refDom RIGHT join chapitre on matiere.id = chapitre.refMat UNION
-select domain.id as idDom,domain.name as nameDom,matiere.id as idMat,matiere.name as nameMat,chapitre.id as idChap,chapitre.name as nameChap from domain LEFT join matiere on domain.id=matiere.refDom LEFT join chapitre on matiere.id = chapitre.refMat ORDER BY idDom, idMat,idChap";
+         $sql ="select Domain.id as idDom,Domain.name as nameDom,Matiere.id as idMat,Matiere.name as nameMat,Chapitre.id as idChap,Chapitre.name as nameChap from Domain RIGHT join Matiere on Domain.id=Matiere.refDom RIGHT join Chapitre on Matiere.id = Chapitre.refMat UNION
+select Domain.id as idDom,Domain.name as nameDom,Matiere.id as idMat,Matiere.name as nameMat,Chapitre.id as idChap,Chapitre.name as nameChap from Domain LEFT join Matiere on Domain.id=Matiere.refDom LEFT join Chapitre on Matiere.id = Chapitre.refMat ORDER BY idDom, idMat,idChap";
           return $this->db->fetchAll($sql);
     }
 
